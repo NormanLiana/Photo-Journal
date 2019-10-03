d6fdfbdffd92d77e5358ef3c66967198ad2d31bb
NormanLiana <liananorman@pursuit.org>
2019-10-03 16:34:46 -0400

    adds and builds PhotoJournal Model and adds cocoa file for CV cell

---
+++ PhotoJournal-Project/Models/PhotoJournal.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoJournal.swift
+//  PhotoJournal-Project
+//
+//  Created by Liana Norman on 10/3/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
+
+struct PhotoJournal: Codable {
+    let photo: Data
+    let title: String
+}
---
+++ PhotoJournal-Project/Views/PhotoJournalCell.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoJournalCell.swift
+//  PhotoJournal-Project
+//
+//  Created by Liana Norman on 10/3/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalCell: UICollectionViewCell {
+    
+}
