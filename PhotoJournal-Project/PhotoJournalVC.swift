a8ec3c30149ea7a3c91ce311ad93b22ee60745c9
NormanLiana <liananorman@pursuit.org>
2019-10-03 15:40:04 -0400

    adds cocoa file for PhotoJournalVC and marks VC for organization

---
+++ PhotoJournal-Project/PhotoJournalVC.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoJournalVC.swift
+//  PhotoJournal-Project
+//
+//  Created by Liana Norman on 10/3/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class PhotoJournalVC: UIViewController {
+
+    // MARK: Outlets
+    
+    // MARK: Properties
+    
+    // MARK: Lifecycle Methods
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    // MARK: Private Methods
+    
+
+}
+
+// MARK: Extensions
+
