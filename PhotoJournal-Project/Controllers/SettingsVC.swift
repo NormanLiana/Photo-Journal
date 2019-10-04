1b372995568c58cc20ec5e87bc90ec06ed6b82e8
NormanLiana <liananorman@pursuit.org>
2019-10-04 17:52:24 -0400

    adds cocoa file for Settings VC and make VC on storyboard subclass to that file

---
+++ PhotoJournal-Project/Controllers/SettingsVC.swift
@@ -0,0 +1,20 @@
+//
+//  SettingsVC.swift
+//  PhotoJournal-Project
+//
+//  Created by Liana Norman on 10/4/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import UIKit
+
+class SettingsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
