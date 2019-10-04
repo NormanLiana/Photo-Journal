63d604cb13857a517b842425211ff192d64dbeb1
NormanLiana <liananorman@pursuit.org>
2019-10-04 14:40:16 -0400

    adds functionality to photo library button

--- PhotoJournal-Project/Controllers/PhotoEntryVC.swift
+++ PhotoJournal-Project/Controllers/PhotoEntryVC.swift
@@ -10,16 +10,34 @@ import UIKit
 
 class PhotoEntryVC: UIViewController {
 
+    // MARK: Outlets
+    @IBOutlet weak var imgOutlet: UIImageView!
+    
+    // MARK: Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
     
     // MARK: Actions
-    
+    @IBAction func photoLibraryButtonPressed(_ sender: UIBarButtonItem) {
+        let imgPicker = UIImagePickerController()
+        imgPicker.delegate = self
+        imgPicker.sourceType = .photoLibrary
+        present(imgPicker, animated: true, completion: nil)
+    }
     
     @IBAction func cancelButtonPressed(_ sender: UIButton) {
         dismiss(animated: true, completion: nil)
     }
     
+}
+
+extension PhotoEntryVC: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let image = info[.originalImage] as? UIImage {
+            imgOutlet.image = image
+        }
+        dismiss(animated: true, completion: nil)
+    }
 }
