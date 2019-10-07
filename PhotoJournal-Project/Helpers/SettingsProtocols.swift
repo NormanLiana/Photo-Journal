5c576300ca2d4c8a4411cacfb7b5af7cdb3b887d
NormanLiana <liananorman@pursuit.org>
2019-10-07 17:16:00 -0400

    sets up protocol file for settings

---
+++ PhotoJournal-Project/Helpers/SettingsProtocols.swift
@@ -0,0 +1,16 @@
+//
+//  SettingsProtocols.swift
+//  PhotoJournal-Project
+//
+//  Created by Liana Norman on 10/7/19.
+//  Copyright © 2019 Liana Norman. All rights reserved.
+//
+
+import Foundation
+
+protocol SettingsDelegate {
+    func darkModeOn()
+    func darkModeOff()
+    func horizontalScroll()
+    func verticalScroll()
+}
